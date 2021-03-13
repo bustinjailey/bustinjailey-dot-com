e594bc7d183706a1bd2590408df7735e1a6abdb6
Justin Bailey <bustinjailey@gmail.com>
2021-03-13 14:13:36 -0800

    Nothing interesting

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App background-image">
       <header className="App-header">
         <p>
           Work In Progress
